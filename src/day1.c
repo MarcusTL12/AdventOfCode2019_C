af75aaf4d83477891bf3dcde5e8f88fa8a0cc729
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-11-28 10:55:05 +0100

    Day1

--- src/day1.c
+++ src/day1.c
@@ -1,5 +1,54 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void d1p1() { printf("Heisann!!\n"); }
+#include "../AoC_C_utils/src/file_util.h"
 
-void d1p2() {}
+static int fuel(int mod_mass) { return mod_mass / 3 - 2; }
+
+void d1p1() {
+    char *inp = file_read_full("input/day1/input");
+
+    int total = 0;
+
+    for (char *c = inp; *c;) {
+        int mod_mass = atoi(c);
+
+        total += fuel(mod_mass);
+
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c) && *c) c++;
+    }
+
+    printf("Total fuel: %i\n", total);
+
+    free(inp);
+}
+
+static int rec_fuel(int mass) {
+    int f = fuel(mass);
+    if (f > 0) {
+        return f + rec_fuel(f);
+    } else {
+        return 0;
+    }
+}
+
+void d1p2() {
+    char *inp = file_read_full("input/day1/input");
+
+    int total = 0;
+
+    for (char *c = inp; *c;) {
+        int mod_mass = atoi(c);
+
+        total += rec_fuel(mod_mass);
+
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c) && *c) c++;
+    }
+
+    printf("Total fuel: %i\n", total);
+
+    free(inp);
+}

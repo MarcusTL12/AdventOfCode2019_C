5dec2486ff9c97d1de6eca705b6d33655f77834f
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-11-30 16:03:00 +0100

    _

--- src/day10.c
+++ src/day10.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +5,7 @@
 #include "../AoC_C_utils/src/bitfield.h"
 #include "../AoC_C_utils/src/file_util.h"
 #include "../AoC_C_utils/src/number_theory.h"
+#include "../AoC_C_utils/src/vector.h"
 
 static char *parse_input(char *filename, size_t *width, size_t *height) {
     char *inp = file_read_full(filename);
@@ -33,8 +33,8 @@ static void print_map(char *map, size_t width, size_t height) {
     }
 }
 
-static size_t amt_visible(char *map, size_t width, size_t height, uint64_t *set,
-                          size_t x, size_t y) {
+static void fill_visible(char *map, size_t width, size_t height, uint64_t *set,
+                         size_t x, size_t y) {
     size_t set_ints = 1 + (width * height - 1) / 64;
     memset(set, 0, set_ints * sizeof(uint64_t));
 
@@ -53,20 +53,20 @@ static size_t amt_visible(char *map, size_t width, size_t height, uint64_t *set,
             }
         }
     }
-
-    return bitfield_count(set, set_ints);
 }
 
 static void findmax(char *map, size_t width, size_t height, size_t *x_,
                     size_t *y_, size_t *max) {
-    uint64_t *set = malloc((1 + (width * height - 1) / 64) * sizeof(uint64_t));
+    size_t set_ints = 1 + (width * height - 1) / 64;
+    uint64_t *set = malloc(set_ints * sizeof(uint64_t));
 
     *max = 0;
 
     for (size_t y = 0; y < height; y++) {
         for (size_t x = 0; x < width; x++) {
             if (map[x + width * y] == '#') {
-                size_t amt = amt_visible(map, width, height, set, x, y);
+                fill_visible(map, width, height, set, x, y);
+                size_t amt = bitfield_count(set, set_ints);
                 if (amt > *max) {
                     *max = amt;
                     *x_ = x;
@@ -92,4 +92,35 @@ void d10p1() {
     free(map);
 }
 
-void d10p2() {}
+static void find_visible(char *map, size_t width, size_t height, size_t x,
+                         size_t y, uint64_t *set, vec_t *target) {
+    fill_visible(map, width, height, set, x, y);
+
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            if (bitfield_get(set, i * width + height)) {
+                
+            }
+        }
+    }
+
+    vec_clear(target);
+}
+
+void d10p2() {
+    size_t width, height;
+    char *map = parse_input("input/day10/input", &width, &height);
+
+    size_t x, y, max;
+
+    findmax(map, width, height, &x, &y, &max);
+
+    vec_t vis_buf = vec_create(2 * sizeof(size_t));
+    size_t set_ints = 1 + (width * height - 1) / 64;
+    uint64_t *set = malloc(set_ints * sizeof(uint64_t));
+
+    find_visible(map, width, height, x, y, &vis_buf, set);
+
+    vec_free(&vis_buf);
+    free(map);
+}

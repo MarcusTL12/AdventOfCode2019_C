10adcc9daae66faf1a2ee1dc94f02e0918e247e9
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-05-14 23:03:38 +0200

    Day 15

--- src/day15.c
+++ src/day15.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+#include "aoc_util/deque.h"
 #include "aoc_util/hashmap.h"
 #include "aoc_util/point.h"
 #include "intcode.h"
 
 typedef struct {
     point2i pos;
-    bool iswall;
-} cell;
+    int64_t data;
+} cell_t;
 
 static const point2i DIRS[] = {(point2i){0, -1}, (point2i){0, 1},
                                (point2i){-1, 0}, (point2i){1, 0}};
@@ -17,7 +18,7 @@ static void mapmaze_rec(intcode_machine *machine, struct hashmap *maze,
                         point2i pos, size_t *pathlen, point2i *target,
                         size_t *targetdist) {
     for (size_t dir = 0; dir < 4; dir++) {
-        cell ncell = {point2i_add(pos, DIRS[dir]), false};
+        cell_t ncell = {point2i_add(pos, DIRS[dir]), false};
         if (!hashmap_get(maze, &ncell)) {
             intcode_send(machine, dir + 1);
             intcode_run(machine);
@@ -25,7 +26,7 @@ static void mapmaze_rec(intcode_machine *machine, struct hashmap *maze,
             intcode_recieve(machine, &response);
             switch (response) {
                 case 0:
-                    ncell.iswall = true;
+                    ncell.data = true;
                     hashmap_set(maze, &ncell);
                     break;
                 case 2:
@@ -48,10 +49,10 @@ static void mapmaze_rec(intcode_machine *machine, struct hashmap *maze,
 
 static void mapmaze(intcode_machine *machine, struct hashmap **maze,
                     point2i *target, size_t *targetdist) {
-    *maze = hashmap_new(sizeof(cell), 0, 2019, 15, point2i_hash, point2i_cmp,
+    *maze = hashmap_new(sizeof(cell_t), 0, 2019, 15, point2i_hash, point2i_cmp,
                         NULL, NULL);
 
-    cell curcell = {{0, 0}, false};
+    cell_t curcell = {{0, 0}, false};
     hashmap_set(*maze, &curcell);
 
     size_t pathlen = 0;
@@ -73,4 +74,40 @@ void d15p1() {
     intcode_free(&machine);
 }
 
-void d15p2() {}
+void d15p2() {
+    intcode_machine machine = intcode_from_file("input/day15/input");
+    struct hashmap *maze;
+    point2i target;
+    size_t targetdist;
+
+    mapmaze(&machine, &maze, &target, &targetdist);
+
+    struct hashmap *visited = hashmap_new(
+        sizeof(point2i), 0, 2019, 15, point2i_hash, point2i_cmp, NULL, NULL);
+    hashmap_set(visited, &target);
+
+    deque_t queue = deque_create(sizeof(cell_t));
+    deque_push_back(&queue, &(cell_t){target, 0});
+
+    int64_t maxlen = 0;
+
+    for (cell_t curcell; deque_pop_front(&queue, &curcell);) {
+        maxlen = curcell.data > maxlen ? curcell.data : maxlen;
+        for (size_t i = 0; i < 4; i++) {
+            point2i npos = point2i_add(curcell.pos, DIRS[i]);
+            if (!hashmap_get(visited, &npos) &&
+                !((cell_t *)hashmap_get(maze, &npos))->data) {
+                hashmap_set(visited, &npos);
+                cell_t ncell = {npos, curcell.data + 1};
+                deque_push_back(&queue, &ncell);
+            }
+        }
+    }
+
+    printf("%li minutes\n", maxlen);
+
+    deque_free(&queue);
+    hashmap_free(visited);
+    hashmap_free(maze);
+    intcode_free(&machine);
+}

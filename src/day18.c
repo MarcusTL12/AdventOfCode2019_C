68702227d8825927ff6d59ec728458958bede903
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-05-19 15:25:48 +0200

    Day 18

--- src/day18.c
+++ src/day18.c
@@ -6,22 +6,22 @@
 #include "aoc_util/deque.h"
 #include "aoc_util/file_util.h"
 #include "aoc_util/hashmap.h"
+#include "aoc_util/hashmap_util.h"
 #include "aoc_util/point.h"
 #include "aoc_util/show.h"
 
-static void load_maze(char *filename, char **maze, size_t *w, size_t *h,
-                      point2i *keys, point2i *doors, size_t *amt_keys,
-                      point2i *start) {
+static void load_maze(char *filename, char **maze, size_t *w, point2i *keys,
+                      point2i *doors, size_t *amt_keys, point2i *start) {
     *maze = file_read_full(filename);
 
     *w = 0;
     for (char *c = *maze; *c != '\n'; c++) (*w)++;
     (*w)++;
-    *h = (strlen(*maze) + 1) / (*w);
+    size_t h = (strlen(*maze) + 1) / (*w);
 
     *amt_keys = 0;
 
-    for (size_t y = 0; y < *h; y++) {
+    for (size_t y = 0; y < h; y++) {
         for (size_t x = 0; x < *w; x++) {
             char c = (*maze)[y * *w + x];
 
@@ -99,7 +99,6 @@ static size_t find_shortest_rec(char *maze, size_t w, point2i start,
     point2i keys[26] = {[0 ... 25] = {0, 0}};
     find_keys_bfs(maze, w, start, queue, keys, collected_keys, visited);
 
-    size_t min_ind = 0;
     size_t min_steps = -1;
     uint64_t new_collected;
     for (size_t i = 0; keys[i].y; i++) {
@@ -112,7 +111,6 @@ static size_t find_shortest_rec(char *maze, size_t w, point2i start,
 
         if (cur_steps < min_steps) {
             min_steps = cur_steps;
-            min_ind = i;
         }
     }
 
@@ -127,10 +125,9 @@ static size_t find_shortest_rec(char *maze, size_t w, point2i start,
 
 void d18p1() {
     char *maze;
-    size_t w, h, amt_keys;
+    size_t w, amt_keys;
     point2i keys[26], doors[26], start;
-    load_maze("input/day18/input", &maze, &w, &h, keys, doors, &amt_keys,
-              &start);
+    load_maze("input/day18/input", &maze, &w, keys, doors, &amt_keys, &start);
 
     deque_t queue = deque_create(sizeof(cell_t));
     struct hashmap *visited = hashmap_new(
@@ -149,4 +146,96 @@ void d18p1() {
     free(maze);
 }
 
-void d18p2() {}
+typedef struct {
+    point2i pos[4];
+    uint64_t collected_keys;
+    size_t steps;
+} memo2_t;
+
+MAKE_HASH_AND_CMP(memo2, 4 * sizeof(point2i) + sizeof(uint64_t))
+
+static size_t find_shortest_rec2(char *maze, size_t w, point2i *starts,
+                                 uint64_t collected_keys, deque_t *queue,
+                                 struct hashmap *visited, struct hashmap *memo,
+                                 point2i *key_pos) {
+    {
+        memo2_t *save = hashmap_get(
+            memo, &(memo2_t){{starts[0], starts[1], starts[2], starts[3]},
+                             collected_keys,
+                             0});
+        if (save) {
+            return save->steps;
+        }
+    }
+
+    size_t minmin_steps = -1;
+
+    for (size_t j = 0; j < 4; j++) {
+        point2i keys[26] = {[0 ... 25] = {0, 0}};
+        find_keys_bfs(maze, w, starts[j], queue, keys, collected_keys, visited);
+
+        size_t min_steps = -1;
+        uint64_t new_collected;
+        for (size_t i = 0; keys[i].y; i++) {
+            new_collected = collected_keys;
+            bitfield_set(&new_collected, keys[i].x, true);
+            point2i new_pos[4] = {starts[0], starts[1], starts[2], starts[3]};
+            new_pos[j] = key_pos[keys[i].x];
+            size_t cur_steps =
+                keys[i].y + find_shortest_rec2(maze, w, new_pos, new_collected,
+                                               queue, visited, memo, key_pos);
+
+            if (cur_steps < min_steps) {
+                min_steps = cur_steps;
+            }
+        }
+
+        if (min_steps < minmin_steps) {
+            minmin_steps = min_steps;
+        }
+    }
+
+    if (minmin_steps == -1) {
+        minmin_steps = 0;
+    }
+
+    hashmap_set(memo, &(memo2_t){{starts[0], starts[1], starts[2], starts[3]},
+                                 collected_keys,
+                                 minmin_steps});
+
+    return minmin_steps;
+}
+
+void d18p2() {
+    char *maze;
+    size_t w, amt_keys;
+    point2i keys[26], doors[26], start;
+    load_maze("input/day18/input", &maze, &w, keys, doors, &amt_keys, &start);
+
+    point2i starts[] = {point2i_add(start, (point2i){1, 1}),
+                        point2i_add(start, (point2i){-1, 1}),
+                        point2i_add(start, (point2i){-1, -1}),
+                        point2i_add(start, (point2i){1, -1})};
+
+    maze[start.x + start.y * w] = '#';
+    for (size_t i = 0; i < 4; i++) {
+        point2i d = DIRS[i];
+        maze[start.x + d.x + (start.y + d.y) * w] = '#';
+    }
+
+    deque_t queue = deque_create(sizeof(cell_t));
+    struct hashmap *visited = hashmap_new(
+        sizeof(point2i), 0, 18, 1, point2i_hash, point2i_cmp, NULL, NULL);
+    struct hashmap *memo = hashmap_new(sizeof(memo2_t), 0, 2019, 18 * 2,
+                                       memo2_hash, memo2_cmp, NULL, NULL);
+
+    size_t ans =
+        find_shortest_rec2(maze, w, starts, 0, &queue, visited, memo, keys);
+
+    printf("%zu\n", ans);
+
+    hashmap_free(memo);
+    hashmap_free(visited);
+    deque_free(&queue);
+    free(maze);
+}

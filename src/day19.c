41583915e219289bc5daf7448eba7fc522023333
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-05-20 15:43:17 +0200

    Day 19 part 1

--- src/day19.c
+++ src/day19.c
@@ -2,37 +2,36 @@
 
 #include "intcode.h"
 
-static bool pulled(intcode_machine *machine, int64_t x, int64_t y) {
-    intcode_machine clone = intcode_clone(machine);
+static bool pulled(intcode_machine *machine, vec_t *program, int64_t x,
+                   int64_t y) {
+    intcode_reset(machine, program);
 
-    intcode_send(&clone, x);
-    intcode_send(&clone, y);
-    intcode_run(&clone);
+    intcode_send(machine, x);
+    intcode_send(machine, y);
+    intcode_run(machine);
 
     int64_t ans;
-    intcode_recieve(&clone, &ans);
-
-    intcode_free(&clone);
+    intcode_recieve(machine, &ans);
 
     return ans;
 }
 
 void d19p1() {
     intcode_machine machine = intcode_from_file("input/day19/input");
+    vec_t program = vec_clone(&machine.program);
 
     int64_t ans = 0;
 
     for (int64_t y = 0; y < 50; y++) {
         for (int64_t x = 0; x < 50; x++) {
-            ans += pulled(&machine, x, y);
+            ans += pulled(&machine, &program, x, y);
         }
     }
 
     printf("%li\n", ans);
 
+    vec_free(&program);
     intcode_free(&machine);
 }
 
-void d19p2() {
-
-}
+void d19p2() {}
